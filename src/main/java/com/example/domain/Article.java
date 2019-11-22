faae881c66fef0f4a303eb72d143f711bb055623
ikedariho <riho.ikeda@rakus-partners.co.jp>
2019-11-22 11:19:14 +0900

    ikedaka Commnet Domain 作成

--- src/main/java/com/example/domain/Article.java
+++ src/main/java/com/example/domain/Article.java
@@ -1,5 +1,71 @@
 package com.example.domain;
 
+import java.util.List;
+
+/**
+ * 記事投稿情報を表すドメイン.
+ * 
+ * @author riho.ikeda
+ *
+ */
 public class Article {
+	/** 投稿ID */
+	private Integer id;
+	/** 名前 */
+	private String name;
+	/** コンテント */
+	private String content;
+	/** コメントリスト */
+	private List<Comment> commentList;
+
+	public Article() {
+		super();
+	}
+
+	public Article(Integer id, String name, String content, List<Comment> commentList) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.content = content;
+		this.commentList = commentList;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public List<Comment> getCommentList() {
+		return commentList;
+	}
+
+	public void setCommentList(List<Comment> commentList) {
+		this.commentList = commentList;
+	}
+
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", name=" + name + ", content=" + content + ", commentList=" + commentList + "]";
+	}
+
 
 }
--- src/main/java/com/example/domain/Comment.java
+++ src/main/java/com/example/domain/Comment.java
@@ -1,6 +1,63 @@
 package com.example.domain;
 
+/**
+ * コメント情報を表すドメイン.
+ * 
+ * @author riho.ikeda
+ *
+ */
 public class Comment {
-
+	/** コメントID */
 	private Integer id;
+	/** 名前 */
+	private String name;
+	/** コンテント */
+	private String content;
+	/** 投稿ID */
+	private Integer articleId;
+
+	public Comment() {
+		super();
+	}
+
+	public Comment(Integer id, String name, String content, Integer articleId) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.content = content;
+		this.articleId = articleId;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Integer getArticleId() {
+		return articleId;
+	}
+
+	public void setArticleId(Integer articleId) {
+		this.articleId = articleId;
+	}
+
 }
